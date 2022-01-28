5ec3290cf32c308031d310c05a929d037dd9dea6
mahbubul14 <mahbub21alam2017@gmail.com>
2022-01-28 13:24:21 +0600

    Fix linters error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
-  def index
-  end
+  def index; end
+
+  def user; end
+
+  def posts; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # get 'users/index'
+  get 'users/user'
+  get 'users/posts'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
