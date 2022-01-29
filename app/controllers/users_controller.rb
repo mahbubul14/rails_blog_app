c2317806a42e68fbee0dd05cd0e9283945417e09
mahbubul14 <mahbub21alam2017@gmail.com>
2022-01-29 15:14:58 +0600

    Complete rails route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,4 @@ class UsersController < ApplicationController
   def index; end
 
   def user; end
-
-  def posts; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  # get 'users/index'
-  get 'users/user'
-  get 'users/posts'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "users#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+  root 'users#index'
 end
